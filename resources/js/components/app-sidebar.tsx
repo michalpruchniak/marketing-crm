2463b17566864d7f9f3fe29837bff0c97a302c85
Michał Pruchniak <m.pruchniak@perspektywy.pl>
2026-09-01 11:19:52 +0200

    delete imports

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,7 +1,6 @@
 import { Link, usePage } from '@inertiajs/react';
-import { BookOpen, FolderGit2, LayoutGrid, UserCog, Users } from 'lucide-react';
+import { LayoutGrid, UserCog, Users } from 'lucide-react';
 import AppLogo from '@/components/app-logo';
-import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import {
